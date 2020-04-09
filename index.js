5a2e23fe30fc3632d0c728d67922b4f2e35d267b
Vishal <vishalnagda1@gmail.com>
2020-04-09 16:53:35 +0530

    update index.js file to render theme.html file

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
-const requestHandler = (request, response) => {};
+const readHTML = (path, response) => {
+    const file = fs.readFileSync(path);
+    if(file) return file;
+    response.end();
+};
 
-http.createServer(requestHandler).listen(3000);
+const requestHandler = (request, response) => {
+    const {pathname, query} = url.parse(request.url);
+
+    switch(pathname) {
+        case '/':
+        case '/about':
+            const file = readHTML("./views/theme.html")
+            response.end(file);
+            break;
+        default:
+            response.end();
+    }
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+    console.log("Server is listening at http://0.0.0.0:3000");
+});
