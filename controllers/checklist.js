21ab240ebd6533fca60e6967f8cb7aa557433258
Vishal <vishalnagda1@gmail.com>
2020-04-10 19:22:30 +0530

    create a basic controller for task model

---
+++ controllers/checklist.js
@@ -0,0 +1,26 @@
+const Task = require("../models/checklist");
+const querystring = require("querystring");
+
+module.exports.create = (request, response) => {
+    let dataQuery = ""
+    request.on("data", data => {dataQuery += data});
+    request.on("end", () => {
+        const {task} = querystring.parse(dataQuery);
+        console.log("task", task);
+        new Task({task}).save((err, data) => {
+            if(err) {
+                console.error(err);
+                response.end("404");
+            } else {
+                console.log(`data`, data);
+                response.writeHead(308, {"Location": "/checklist"});
+                response.end();
+            }
+        }); 
+    });
+}
+
+module.exports.getTasks = async (complete) => {
+    const tasks = await Task.find({complete});
+    return tasks;
+}
