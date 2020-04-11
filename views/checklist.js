8e5542c6057452af7c7777c46c6c219390005410
Vishal <vishalnagda1@gmail.com>
2020-04-11 16:13:01 +0530

    1. update checklist view to make its-done and delete button functional 2. disable ALL DONE button when there is no task.

--- views/checklist.js
+++ views/checklist.js
@@ -9,21 +9,23 @@ exports.view = async (request, response) => {
         taskData += `<tr>
                     <td>${index+1}. ${task.task} (Created at ${new Date(task.createdAt).toDateString()})</td>
                     <td>
-                        <div class="btn-group" role="group" aria-label="Basic example">
-                            <button type="button" dataId="${task._id}" class="btn btn-warning task-done">Its Done</button>
-                            <button type="button" dataId="${task._id}" class="btn btn-danger task-delete">Delete</button>
+                        <div class="btn-group" role="group" aria-label="task-operations">
+                            <button type="button" dataId="${task._id}" class="btn btn-warning" onclick="markDone(this)">Its Done</button>
+                            <button type="button" dataId="${task._id}" class="btn btn-danger" onclick="deleteTask(this)">Delete</button>
                         </div>
                     </td>
                 </tr>`;
     });
+
+    const disabled = taskData ? "" : "disabled";
     
     const data = `<table class="table table-dark">
         <tr>
             <td><input type="text" class="form-control" id="taskInput" aria-label="Task input"
                     aria-describedby="inputGroup-sizing-sm" /></td>
             <td>
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" onclick="allDone()" class="btn btn-warning">All Done</button>
+                <div class="btn-group" role="group" aria-label="task-operation">
+                    <button type="button" onclick="allDone()" class="btn btn-warning" ${disabled}>All Done</button>
                     <button type="button" onclick="addNewTask()" class="btn btn-success">Add New</button>
                 </div>
             </td>
